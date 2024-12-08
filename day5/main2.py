569b5e659b0615fc48481945ffc4a797016a7474
Mason Acevedo <mason.acevedo2000@gmail.com>
2024-12-08 12:38:23 -0800

    day 5 part 2 working

--- day5/main2.py
+++ day5/main2.py
@@ -2,8 +2,6 @@ import sys
 import copy
 
 def isUpdateCorrect(update, rule):
-    earlyNum, lateNum = rule.split("|")
-    nums = update.split(",")
     if not(earlyNum in update) or not(lateNum in update):
         return True
 
@@ -17,38 +15,78 @@ def validUpdate(update, rules):
             return False
     return True
 
+def reorderOneRule(update, rule):
+    earlyNum, lateNum = rule
+    if not(earlyNum in update) or not(lateNum in update):
+        return
+
+    earlyNumIndex = update.index(earlyNum)
+    lateNumIndex = update.index(lateNum)
+
+    if earlyNumIndex > lateNumIndex:
+        temp = update[earlyNumIndex]
+        update[earlyNumIndex] = update[lateNumIndex]
+        update[lateNumIndex] = temp
+
+
+def reorderAllRules(update, rules):
+    answer = copy.deepcopy(update)
+    # given an update,
+    # we loop through the rules, and 
+    # we change the update whenever
+    # the update violates some rule.
+    # The smart thing to do would be 
+    # to loop through the rules and change
+    # the update as long as the update is 
+    # invalid.
+    # The dumb thing to do is just loop
+    # through the rules a lot, then increase
+    # the number, and if the result doesn't change,
+    # you know you've probably looped enough.
+    # that's what we do here. 
+    for i in range(0, 100):
+        for rule in rules:
+            reorderOneRule(answer, rule)
+    return answer
+
+def parseRule(ruleString):
+    return ruleString.split("|")
+
+def parseUpdate(update):
+    return update.split(",")
+
 def main(file_name):
     with open(file_name) as file_handle:
         lines = file_handle.readlines()
     table = [line.replace("\n","") for line in lines]
-    rules = []
-    updates = []
+    ruleStrings = []
+    updateStrings = []
     pastRulesSection = False
 
     for line in table:
         if pastRulesSection:
-            updates.append(line)
+            updateStrings.append(line)
         else:
-            rules.append(line)
+            ruleStrings.append(line)
         if line == '':
             pastRulesSection = True
-    rules.pop()
+    ruleStrings.pop()
+    
+    rules = list(map(parseRule, ruleStrings))
+    updates = list(map(parseUpdate, updateStrings))
 
-    validUpdates = list(filter(lambda f: validUpdate(f, rules), updates))
 
-    validUpdatesAsList = [updateString.split(",") for updateString in validUpdates]
-#     for update in validUpdatesAsList:
-#         print(update)
+    newRules = []
+    for update in updates:
+        result = reorderAllRules(update, rules)
+        if result == update:
+            pass
+        else:
+            newRules.append(result)
 
-    middleNums = [int(update[len(update)//2]) for update in validUpdatesAsList]
 
+    middleNums = [int(newRule[len(newRule)//2]) for newRule in newRules]
     return sum(middleNums)
-
-
-
-#     return horizCount(table) + vertCount(table) + diagCount(table) + diagCount(flipped_table)
-
-
 
 if __name__ == "__main__":
     print(main(sys.argv[1]))
