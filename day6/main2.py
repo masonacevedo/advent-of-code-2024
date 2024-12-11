befcbd80236ab4b89f6eccb658288fd6d189a4cb
Mason Acevedo <macevedo@purestorage.com>
2024-12-11 15:46:10 -0800

    day 6 part 1 example working

--- day6/main2.py
+++ day6/main2.py
@@ -1,6 +1,6 @@
 import sys
 import copy
-
+import pprint
 from enum import Enum
 
 class Direction(Enum):
@@ -9,20 +9,50 @@ class Direction(Enum):
     SOUTH = 3
     WEST = 4
 
+myDict = {
+    Direction.NORTH: Direction.EAST,
+    Direction.EAST: Direction.SOUTH,
+    Direction.SOUTH: Direction.WEST,
+    Direction.WEST: Direction.NORTH,
+}
+
+def turn90(direction):
+    return myDict.get(direction)    
+
+def nextPos(currentPos, direction):
+    row, col = currentPos 
+
+    if direction == Direction.NORTH:
+        return [row-1, col]
+    elif direction == Direction.SOUTH:
+        return [row+1, col]
+    elif direction == Direction.WEST:
+        return [row, col-1]
+    elif direction == Direction.EAST:
+        return [row, col+1]
+
 def updateGrid(grid, direction, currentPos):
-
-	row, col = currentPos
-	nextRowForwards, nextColForwards = nextPos(currentPos, direction)
+    # print("currentPos, beginning:", currentPos)
+    row, col = currentPos
+    nextRowForwards, nextColForwards = nextPos(currentPos, direction)
 	
-	if itemAtLocation(grid, nextRowForwards, nextColForwards) == "#":
-		direction = turn90(direction)
-		nextRowTurned, nextColTurned = nextPos(currentPos, direction)
-		currentPos = [nextRowTurned, nextColTurned] 
-	else:
-		currentPos = [nextRowForwards, nextColForwards]
+    if grid[nextRowForwards][nextColForwards] == "#":
+        direction = turn90(direction)
+        nextRowTurned, nextColTurned = nextPos(currentPos, direction)
+        if grid[nextRowTurned][nextColTurned] == "#":
+            direction = turn90(direction)
+            nextRowTurned, nextColTurned = nextPos(currentPos, direction)
+        currentPos[0] = nextRowTurned
+        currentPos[1] = nextColTurned
+    else:
+        currentPos[0] = nextRowForwards
+        currentPos[1] = nextColForwards
 
-	grid[row][col] = "X"
-	return direction
+    grid[currentPos[0]][currentPos[1]] = "^"
+    
+    grid[row][col] = "X"
+    # print("currentPos, ending:", currentPos)
+    return direction
 
 def nextStepInBounds(grid, direction, currentPosition):
     row, col = currentPosition
@@ -37,15 +67,15 @@ def nextStepInBounds(grid, direction, currentPosition):
     raise Exception("Fill this in")
 
 def loops(grid, direction, position):
-    n = 1000
+    n = 10000
     i = 0
     while(i < n) and nextStepInBounds(grid, direction, position):
         i += 1
-        try:
-            direction, position = updateGrid(grid, direction)
-        except IndexError:
-            return False
-    return True
+        direction = updateGrid(grid, direction, position)
+        # pprint.pprint(grid)
+        # input()
+
+    return nextStepInBounds(grid, direction, position)
 
 def generatePotentialGrids(grid):
     ans = []
@@ -61,7 +91,7 @@ def generatePotentialGrids(grid):
 def findStartingPositino(grid):
     for rowIndex in range(0, len(grid)):
         for colIndex in range(0,len(grid[0])):
-            if grid[rowIndex][colIndex]:
+            if grid[rowIndex][colIndex] == "^":
                 return [rowIndex, colIndex]
     raise Exception("it shouldn't be possible to get here")
 
